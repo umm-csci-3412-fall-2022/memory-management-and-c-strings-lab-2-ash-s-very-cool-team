9eb872aa3f7b30431f3e5725e2c73129ef7048f8
Ash <plase024@morris.umn.edu>
2022-09-22 16:00:34 -0500

    implement disemvowel

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -1,7 +1,32 @@
 #include <stdlib.h>
+#include <string.h>
 
 #include "disemvowel.h"
 
 char *disemvowel(char *str) {
-  return (char*) "";
+  // Allocate space for the result with the same length as the input string plus one for the null terminator
+  char* result = (char*)calloc(strlen(str) + 1, 1);
+
+  // Index into the result string
+  unsigned int i = 0;
+
+  // Iterate over each character of the input string
+  do {
+    // If it is a vowel, go to the next character
+    switch (*str) {
+      case 'a': case 'A':
+      case 'e': case 'E':
+      case 'i': case 'I':
+      case 'o': case 'O':
+      case 'u': case 'U':
+        break;
+      // Otherwise, copy the character to the output
+      default:
+        result[i++] = *str;
+        break;
+    }
+  }
+  while (*(str++));
+
+  return result;
 }
